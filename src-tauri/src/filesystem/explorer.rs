71d3ed5e1605f18dff045fa64d935495736856e3
Nathaniel Richards <nthnlrichards@gmail.com>
2023-09-13 13:04:54 -0400

    fixed issue with refreshes fetching root volume

--- src-tauri/src/filesystem/explorer.rs
+++ src-tauri/src/filesystem/explorer.rs
@@ -2,6 +2,7 @@ use crate::error::{Error, GitError};
 use crate::StateSafe;
 use clipboard::{ClipboardContext, ClipboardProvider};
 use futures::future;
+use rayon::prelude::{ParallelBridge, ParallelIterator};
 use serde::Serialize;
 use std::borrow::Cow;
 use std::collections::HashMap;
@@ -321,37 +322,17 @@ pub async fn delete_file(
 async fn fetch_directory(path: String) -> io::Result<Vec<DirectoryChild>> {
     let directory = fs::read_dir(&path)?;
 
-    let tasks: Vec<_> = directory
-        .filter_map(|entry| {
-            match entry {
-                Ok(entry) => Some(task::spawn(handle_entry(entry))),
-                Err(_) => None, // You can handle errors more gracefully if needed.
+    let mut results = Vec::new();
+    for entry in directory.filter_map(Result::ok) {
+        match handle_entry(entry).await {
+            Ok(child_vec) => results.extend(child_vec),
+            Err(e) => {
+                // Handle or log the error as needed.
             }
-        })
-        .collect();
-
-    let task_results: Result<Vec<_>, _> = future::try_join_all(tasks).await;
-
-    match task_results {
-        Ok(results) => {
-            let mut final_results = Vec::new();
-
-            for res in results {
-                match res {
-                    Ok(child_vec) => final_results.extend(child_vec),
-                    Err(_e) => {
-                        // Handle or log individual task errors as needed.
-                    }
-                }
-            }
-
-            Ok(final_results)
-        }
-        Err(join_err) => {
-            // Handle the JoinError if needed
-            Err(io::Error::new(io::ErrorKind::Other, join_err.to_string()))
         }
     }
+
+    Ok(results)
 }
 
 async fn handle_entry(entry: DirEntry) -> io::Result<Vec<DirectoryChild>> {
